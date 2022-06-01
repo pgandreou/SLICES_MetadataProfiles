bf8cab770e713de1b4d7f7f87621af666216cd1c
Xymanek <xymanek@outlook.com>
2022-06-01 13:24:22 +0300

    Basic SlicesCodeAttribute implementation

---
+++ Slices.V1.FormatAnnotations.Tests/SlicesCodeTest.cs
@@ -0,0 +1,14 @@
+namespace Slices.V1.FormatAnnotations.Tests;
+
+public class SlicesCodeTest
+{
+    [Theory]
+    [InlineData(SlicesFieldCategory.PrimaryInformation, 1, "SLICES.PI.01")]
+    [InlineData(SlicesFieldCategory.ManagementInformation, 12, "SLICES.MA.12")]
+    public void ToCodeStringDefaultDomain(SlicesFieldCategory category, int order, string expected)
+    {
+        SlicesCodeAttribute slicesCode = new(category, order);
+
+        Assert.Equal(expected, slicesCode.ToCodeString());
+    }
+}
---
+++ Slices.V1.FormatAnnotations/SlicesCode.cs
@@ -0,0 +1,45 @@
+﻿namespace Slices.V1.FormatAnnotations;
+
+public enum SlicesFieldCategory
+{
+    /// <summary>
+    /// Important information that is used to identify and describe the resource
+    /// </summary>
+    PrimaryInformation,
+
+    /// <summary>
+    /// Information related to the management of the S-FDO including the
+    /// versioning and metadata profile used to interpret it
+    /// </summary>
+    ManagementInformation,
+
+    // TODO rest
+};
+
+[AttributeUsage(AttributeTargets.Property)]
+public class SlicesCodeAttribute : Attribute
+{
+    public readonly SlicesFieldCategory Category;
+    public readonly int Order;
+
+    public string Domain { get; set; } = "SLICES";
+
+    public SlicesCodeAttribute(SlicesFieldCategory category, int order)
+    {
+        Category = category;
+        Order = order;
+    }
+
+    public string ToCodeString() => $"{Domain}.{Category.ToCodeString()}.{Order:D2}";
+}
+
+public static class SlicesCodeExtensions
+{
+    public static string ToCodeString(this SlicesFieldCategory category) => category switch
+    {
+        SlicesFieldCategory.PrimaryInformation => "PI",
+        SlicesFieldCategory.ManagementInformation => "MA",
+
+        _ => throw new ArgumentOutOfRangeException(nameof(category), category, "Unhanded field category"),
+    };
+}
