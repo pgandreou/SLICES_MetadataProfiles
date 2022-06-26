e0c761c929e7d9dee48ea77df8e35bb51feb701a
Xymanek <xymanek@outlook.com>
2022-06-26 13:13:21 +0300

    Destination standard/format hookup

--- Slices.ConverterGUI/ViewModels/MainWindowViewModel.cs
+++ Slices.ConverterGUI/ViewModels/MainWindowViewModel.cs
@@ -38,6 +38,9 @@ public class MainWindowViewModel : ViewModelBase
 
     private DropdownOption? _selectedSourceStandard;
     private DropdownOption? _selectedSourceFormat;
+    
+    private DropdownOption? _selectedDestinationStandard;
+    private DropdownOption? _selectedDestinationFormat;
 
     private string _destinationValue = "";
 
@@ -53,41 +56,54 @@ public class MainWindowViewModel : ViewModelBase
         }
 
         SourceFormatPlaceholder = this.WhenAnyValue(x => x.SelectedSourceStandard)
-            .Select(option => option is null ? "Please select the standard" : "Click to select");
+            .Select(StandardToFormatPlaceholder);
 
         SourceFormats = this.WhenAnyValue(x => x.SelectedSourceStandard)
-            .Select(standardOption =>
-            {
-                if (standardOption is null)
-                {
-                    return Array.Empty<DropdownOption>();
-                }
+            .Select(StandardToFormatOptions);
+        
+        DestinationFormatPlaceholder = this.WhenAnyValue(x => x.SelectedDestinationStandard)
+            .Select(StandardToFormatPlaceholder);
 
-                return _converterCollection.CovertersByStandard[standardOption.Id]
-                    .SupportedFormats.Formats
-                    .Where(descriptor => descriptor.IsText) // Currently we have no way to use binary data in the UI
-                    .Select(descriptor => new DropdownOption
-                    {
-                        Id = descriptor.FormatId,
-                        Label = descriptor.FormatId
-                    })
-                    .ToArray();
-            });
+        DestinationFormats = this.WhenAnyValue(x => x.SelectedDestinationStandard)
+            .Select(StandardToFormatOptions);
 
         IObservable<bool> convertEnabled = this
             .WhenAnyValue(
                 x => x.SelectedSourceStandard,
-                x => x.SelectedSourceFormat
+                x => x.SelectedSourceFormat,
+                
+                x => x.SelectedDestinationStandard,
+                x => x.SelectedDestinationFormat
             )
             .Select(tuple =>
                 tuple.Item1 is not null &&
-                tuple.Item2 is not null
+                tuple.Item2 is not null &&
+                tuple.Item3 is not null &&
+                tuple.Item4 is not null
             );
 
         Convert = ReactiveCommand.Create(
             () => { DestinationValue += "1"; },
             convertEnabled
         );
+    }
+
+    private static string StandardToFormatPlaceholder(DropdownOption? option)
+    {
+        return option is null ? "Please select the standard" : "Click to select";
+    }
+
+    private DropdownOption[] StandardToFormatOptions(DropdownOption? standardOption)
+    {
+        if (standardOption is null)
+        {
+            return Array.Empty<DropdownOption>();
+        }
+
+        return _converterCollection.CovertersByStandard[standardOption.Id]
+            .SupportedFormats.Formats.Where(descriptor => descriptor.IsText) // Currently we have no way to use binary data in the UI
+            .Select(descriptor => new DropdownOption { Id = descriptor.FormatId, Label = descriptor.FormatId })
+            .ToArray();
     }
 
     public ObservableCollection<DropdownOption> Standards { get; } = new();
@@ -106,6 +122,22 @@ public class MainWindowViewModel : ViewModelBase
     {
         get => _selectedSourceFormat;
         set => this.RaiseAndSetIfChanged(ref _selectedSourceFormat, value);
+    }
+    
+    public IObservable<string> DestinationFormatPlaceholder { get; }
+
+    public IObservable<DropdownOption[]> DestinationFormats { get; }
+    
+    public DropdownOption? SelectedDestinationStandard
+    {
+        get => _selectedDestinationStandard;
+        set => this.RaiseAndSetIfChanged(ref _selectedDestinationStandard, value);
+    }
+
+    public DropdownOption? SelectedDestinationFormat
+    {
+        get => _selectedDestinationFormat;
+        set => this.RaiseAndSetIfChanged(ref _selectedDestinationFormat, value);
     }
 
     public ReactiveCommand<Unit, Unit> Convert { get; }
