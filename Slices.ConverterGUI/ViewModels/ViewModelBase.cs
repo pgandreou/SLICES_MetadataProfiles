434af65f233242d645a4e50573f7c651197a1efb
Xymanek <xymanek@outlook.com>
2022-06-26 10:04:50 +0300

    ConverterGUI init

---
+++ Slices.ConverterGUI/ViewModels/ViewModelBase.cs
@@ -0,0 +1,11 @@
+using ReactiveUI;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Slices.ConverterGUI.ViewModels
+{
+    public class ViewModelBase : ReactiveObject
+    {
+    }
+}
